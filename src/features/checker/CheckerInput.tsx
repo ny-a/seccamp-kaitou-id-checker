6d662ac1e6345a6ce745de5a6e07c3a76478a44a
ny-a <58758248+ny-a@users.noreply.github.com>
2021-06-13 22:40:32 +0900

    Add source links

--- src/features/checker/CheckerInput.tsx
+++ src/features/checker/CheckerInput.tsx
@@ -6,12 +6,15 @@ import styles from './Checker.module.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import { idLists } from './ids';
 
 export function CheckerInput() {
   const dispatch = useAppDispatch();
   const [newId, setNewId] = useState('');
 
   const newIdValue = String(newId) || '';
+
+  const sourceList = idLists.map(idList => (<div><a href={idList.url}>{idList.name} 公式 PDF</a><br /></div>));
 
   return (
     <div>
@@ -27,7 +30,7 @@ export function CheckerInput() {
           </Grid>
           <Grid>
             <Button variant="contained" onClick={() => dispatch(setId(newIdValue))}>
-              確認する
+              通過しているか確認する
             </Button>
           </Grid>
         </form>
@@ -35,7 +38,9 @@ export function CheckerInput() {
       <div>
         <aside className={styles.aside}>
           回答IDは外部に送信していません。<br />
-          (不安であれば Incognito モードで開いて、オフラインにした後で確認してください。)
+          (不安であれば Incognito モードで開いて、オフラインにした後で確認してください。)<br />
+          <br />
+          {sourceList}
         </aside>
       </div>
     </div>
